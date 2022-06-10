fbfcc8a06d3cff3fc1aa6a3fdfd714b4ec571b2f
mariaprdd <mpardede18@gmail.com>
2022-06-11 01:51:54 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter import LabelFrame
+from flask import Flask
+import tensorflow as tf
+import numpy as np
+from keras.preprocessing import image
+import os
+
+from model import preprocess_image
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+    return "Hello"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    model = tf.keras.models.load_model('model10june.h5')
+    img = image.open('./image kulit.jpg')
+    img = preprocess_image
+    prediction = LabelFrame[np.argmax(prediction)]
+    confident = prediction[0][np.argmax(prediction)]
+    print("terdeteksi penyakit {} dengan tingkat kepercayaan {}")
+    
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port='80')
+
---
+++ model.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import tensorflow as tf
+import numpy as np
+from keras.preprocessing import image
+import os
+
+
+model = tf.keras.models.load_model('model10june.h5')
+
+def preprocess_image(image_path, target_size):
+    img = image.preprocess_image(image_path, target_size(224, 224))
+    img_array = image.img_to_array(img)/225
+    img_batch = np.expand_dims(img_array,axis=0)
+    prediction = model.predict(img_batch)
+
+def labels_image(labels_path):
+    labels =  np.array(open(labels_path).read().splitlines())
+    prediction = labels[np.argmax(prediction)]
+    confident = prediction[0][np.argmax(prediction)]
+ 
+print("terdeteksi penyakit {} dengan tingkat kepercayaan {}")
